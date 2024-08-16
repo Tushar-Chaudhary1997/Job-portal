e8f2bbcff10c58af98a78f361696f644e394db43
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-08-16 13:11:50 +0530

    "Added dotenv and database connection, updated port configuration"

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
 
 app.get("/home", (req, res) => {
@@ -18,7 +21,8 @@ const corsOptions = {
   credential: true,
 };
 app.use(cors(corsOptions));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
