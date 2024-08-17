c8117b7309138eb2e9e4e802fe2f18927b7233f3
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-08-17 08:47:21 +0530

    "Added user route to backend and implemented authentication middleware for profile update"

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { register, login, updateProfile } from "../controllers/user.controller";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/profile/update").post(updateProfile);
+router.route("/profile/update").post(isAuthenticated, updateProfile);
+
+export default router;
